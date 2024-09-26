0b3ac047d081c588c832a7e66234d4c90a2ec372
DaniilPozniakv <142602929+DaniilPozniakov@users.noreply.github.com>
2024-09-26 17:35:38 +0300

    restruct: tcp -> ssl, add struct:ConnectionSetting

--- include/WeatherClient.h
+++ include/WeatherClient.h
@@ -1,23 +1,42 @@
+#pragma once
 #include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
 #include <string>
+
+using namespace boost::asio;
+using namespace boost::asio::ssl;
+
+
 
 namespace
 {
-class TCPClient
+struct ConnectionSetting
 {
-private:
-    boost::asio::io_context         _io_context;
-    boost::asio::ip::tcp::socket    _socket;
-    boost::asio::ip::tcp::resolver  _resolver;
-
     std::string _key;
     std::string _host;
     std::string _port;
 
-    void read_config();
+    bool isValid() const noexcept;
+};
 
+
+class Client
+{
 private:
-    TCPClient();
+    io_service                      _io_service;
+    ssl::context                    _ssl_context;
+    ssl::stream<ip::tcp::socket>    _ssl_socket;
+    ip::tcp::resolver               _resolver;
+
+    ConnectionSetting _connectionSetting;
+
+    void read_config();
+    void connect();
+
+public:
+    Client();
+
+
 
 
 };
